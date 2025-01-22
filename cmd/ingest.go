cd1ff4d5babb72395e15976b950f4a5c3798d233
Corentin Barreau <corentin@archive.org>
2025-01-22 10:35:28 +0100

    add: batch ingest of CDX records

--- cmd/ingest.go
+++ cmd/ingest.go
@@ -5,7 +5,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var concurrency int
+var (
+	concurrency int
+	URL         string
+)
 
 // ingestCmd represents the ingest command
 var ingestCmd = &cobra.Command{
@@ -13,11 +16,12 @@ var ingestCmd = &cobra.Command{
 	Short: "Ingest one or many CDX files",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		ingest.Files(concurrency, args...)
+		ingest.Files(concurrency, URL, args...)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(ingestCmd)
 	ingestCmd.Flags().IntVarP(&concurrency, "concurrency", "c", 1, "Number of concurrent files to process")
+	ingestCmd.Flags().StringVarP(&URL, "url", "u", "http://localhost:8080", "Doppelganger server URL")
 }
--- pkg/ingest/ingest.go
+++ pkg/ingest/ingest.go
@@ -7,10 +7,20 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/internetarchive/doppelganger/pkg/client"
+	"github.com/internetarchive/doppelganger/pkg/server/models"
 	"github.com/internetarchive/gocdx"
 )
 
-func Files(concurrency int, files ...string) {
+var (
+	BATCH_SIZE          = 1000
+	MINIMUM_RECORD_SIZE = int64(2000)
+)
+
+func Files(concurrency int, URL string, files ...string) {
+	c := client.NewClient(URL)
+
+	var batch []*models.Record
 	for _, file := range files {
 		file, err := os.Open(file)
 		if err != nil {
@@ -32,12 +42,15 @@ func Files(concurrency int, files ...string) {
 		deduplicatedRecords, dedupedCount := deduplicateRecords(records)
 
 		for _, record := range deduplicatedRecords {
-			if record.StatusCode == 429 || record.StatusCode == 0 {
+			// Skip records with status code 429 or 0
+			// 0 will de-facto skip revisit records.
+			if record.StatusCode == 429 ||
+				record.StatusCode == 0 ||
+				record.CompressedRecordSize < MINIMUM_RECORD_SIZE {
 				atomic.AddInt64(&skipped, 1)
 				continue
 			}
 
-			//slog.Info("CDX record", "url", record.OriginalURL, "timestamp", record.Timestamp, "status", record.StatusCode)
 			atomic.AddInt64(&valid, 1)
 		}
 
@@ -50,7 +63,37 @@ func Files(concurrency int, files ...string) {
 			"total", len(records),
 			"unique", len(deduplicatedRecords),
 		)
+
+		// Divide the records into batches of BATCH_SIZE
+		for i := 0; i < len(deduplicatedRecords); i += BATCH_SIZE {
+			batch = convertToModelRecords(deduplicatedRecords[i:min(i+BATCH_SIZE, len(deduplicatedRecords))])
+			// Add the batch to the server
+			if err := c.AddRecords(batch...); err != nil {
+				fmt.Println("Error adding records:", err)
+				return
+			}
+		}
+
+		// Add the remaining records to the server
+		if len(batch) > 0 {
+			if err := c.AddRecords(batch...); err != nil {
+				fmt.Println("Error adding records:", err)
+				return
+			}
+		}
 	}
+}
+
+func convertToModelRecords(records []gocdx.Record) []*models.Record {
+	modelRecords := make([]*models.Record, len(records))
+	for i, record := range records {
+		modelRecords[i] = &models.Record{
+			ID:   record.NewStyleChecksum,
+			URI:  record.OriginalURL,
+			Date: record.Timestamp.Format("20060102150405"),
+		}
+	}
+	return modelRecords
 }
 
 func deduplicateRecords(records []gocdx.Record) ([]gocdx.Record, int) {
