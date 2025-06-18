dd73671f948d9ad3d0fa90de8fbbd4b984110034
Jake LaFountain <jakelf@archive.org>
2025-06-17 20:15:54 -0400

    Remove log.

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ var Commit = func() string {
 }()
 
 func main() {
-	println(os.Getenv("PYROSCOPE_ADDRESS"))
 	if pyroscopeAddr := os.Getenv("PYROSCOPE_ADDRESS"); pyroscopeAddr != "" {
 		pyroscope.Start(pyroscope.Config{
 			ApplicationName: "doppelganger",
